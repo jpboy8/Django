d3305e35dff289a069713075dc5958c530b8fed2
jpboy8 <101652443+jpboy8@users.noreply.github.com>
2023-02-05 20:03:26 +0600

    add documentation

--- rest_framework/bank/config/settings.py
+++ rest_framework/bank/config/settings.py
@@ -41,8 +41,9 @@ INSTALLED_APPS = [
     'rest_framework',
     'djoser',
     'rest_framework.authtoken',
+    'drf_yasg',
     # local
-    'bank'
+    'bank',
 ]
 
 
--- rest_framework/bank/config/urls.py
+++ rest_framework/bank/config/urls.py
@@ -15,6 +15,22 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include, re_path
+from rest_framework import permissions  # new
+from drf_yasg.views import get_schema_view  # new
+from drf_yasg import openapi
+
+
+schema_view = get_schema_view(  # new
+    openapi.Info(
+        title="Blog API",
+        default_version="v1",
+        description="A sample API for learning DRF",
+        contact=openapi.Contact(email="qwerty@example.com"),
+        license=openapi.License(name="License"),
+    ),
+    public=True,
+    permission_classes=(permissions.AllowAny,),
+)
 
 urlpatterns = [
     path('admin/', admin.site.urls),
@@ -22,4 +38,8 @@ urlpatterns = [
     path('api/bank/', include('bank.urls')),
     path('api/bank/auth/', include('djoser.urls')),
     re_path(r'^auth/', include('djoser.urls.authtoken')),
+    path('swagger/', schema_view.with_ui(  # new
+        'swagger', cache_timeout=0), name='schema-swagger-ui'),
+    path('redoc/', schema_view.with_ui(  # new
+        'redoc', cache_timeout=0), name='schema-redoc'),
 ]
