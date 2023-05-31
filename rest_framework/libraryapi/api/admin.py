8655556c758b1da18443ff5f2a4dd9274176eb20
jpboy8 <sick2002@mail.ru>
2023-05-31 22:24:54 +0600

    Re-Adding ignored file by force

--- rest_framework/libraryapi/api/admin.py
+++
@@ -1,5 +0,0 @@
-from django.contrib import admin
-from .models import Book, Customer, Account, Order, Cart, Cartitems
-
-
-admin.site.register([Book, Customer, Account, Order, Cart, Cartitems])
--- rest_framework/libraryapi/api/permissions.py
+++
@@ -1,5 +0,0 @@
-from rest_framework.permissions import IsAuthenticated
-
-class IsNotAuthenticated(IsAuthenticated):
-    def has_permission(self, request, view):
-        return not request.user.is_authenticated
